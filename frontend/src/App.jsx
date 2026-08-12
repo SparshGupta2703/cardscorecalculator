97cab050fcf52e2baf946fc9f37bdd991ffb318a
sparsh gupta <mr.sparshgupta@gmail.com>
2026-08-12 23:00:23 +0530

    finallay

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { io } from 'socket.io-client';
 import './App.css';
 
-const SOCKET_URL = 'https://cardscorecalculator.onrender.com:4000';
+const SOCKET_URL = import.meta.env.VITE_SOCKET_URL;
 const socket = io(SOCKET_URL);
 
 // Special Component to prevent cursor jumping while editing names in real-time
